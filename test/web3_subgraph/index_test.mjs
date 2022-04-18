c08fd934117b8a578a5012fb8685cef383395ec9
Tim Daubenschütz <tim@daubenschuetz.de>
2022-04-18 14:15:27 +0200

    Fix tests

--- test/web3_subgraph/index_test.mjs
+++ test/web3_subgraph/index_test.mjs
@@ -4,28 +4,28 @@ import test from "ava";
 import { toCSV, toJSON } from "../../src/web3_subgraph/index.mjs";
 
 test("tranform from original => JSON => CSV", (t) => {
-	const list = [{ id: "abc/1" }, { id: "def/2" }];
-	const result = toCSV(toJSON(list));
-	t.is("abc,1\ndef,2", result);
+  const list = [{ id: "abc/1" }, { id: "def/2" }];
+  const result = toCSV(toJSON(list));
+  t.is("abc,1\ndef,2", result);
 });
 
 test("transforming to JSON", (t) => {
-	const list = [{ id: "abc/1" }, { id: "def/2" }];
-	const result = toJSON(list);
-	t.deepEqual(
-		[
-			{ address: "abc", tokenId: "1" },
-			{ address: "def", tokenId: "2" },
-		],
-		result
-	);
+  const list = [{ id: "abc/1" }, { id: "def/2" }];
+  const result = toJSON(list);
+  t.deepEqual(
+    [
+      { address: "abc", tokenId: "1" },
+      { address: "def", tokenId: "2" },
+    ],
+    result
+  );
 });
 
 test("transforming to CSV", (t) => {
-	const list = [
-		{ address: "abc", tokenId: "1" },
-		{ address: "def", tokenId: "2" },
-	];
-	const result = toCSV(list);
-	t.is("abc,1\ndef,2", result);
+  const list = [
+    { address: "abc", tokenId: "1" },
+    { address: "def", tokenId: "2" },
+  ];
+  const result = toCSV(list);
+  t.is("abc,1\ndef,2", result);
 });
