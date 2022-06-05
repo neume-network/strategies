9437724b8ed64ce1408eb3b9e29b1cfdd662f9a7
il3ven <vaibhavchanana@yahoo.co.in>
2022-06-05 16:33:05 +0530

    add test cases for extractor component

---
+++ test/strategies/soundxyz-get-tokenuri/extractor_test.mjs
@@ -0,0 +1,20 @@
+// @format
+import fs from "fs";
+import { resolve, dirname } from "path";
+import { fileURLToPath } from "url";
+import test from "ava";
+import snapshotExtractor from "../../../utils/snapshot_extractor.mjs";
+
+import * as soundxyz from "../../../src/strategies/soundxyz-get-tokenuri/extractor.mjs";
+
+test("soundxyz extractor", async (t) => {
+  const __dirname = dirname(fileURLToPath(import.meta.url));
+
+  const snapshot = JSON.parse(
+    fs.readFileSync(resolve(__dirname, "./extractor_snapshot.json"))
+  );
+
+  snapshot.inputs[0] = resolve(__dirname, snapshot.inputs[0]);
+
+  t.true(await snapshotExtractor(soundxyz, snapshot));
+});
