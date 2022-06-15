abe666b10b1b36bfbd86db7ad37ad46d3f5f89d6
Tim Daubenschütz <tim@daubenschuetz.de>
2022-06-15 18:38:08 +0200

    Add `tokenURI` to catalog-get-tokenuri strategy

--- test/strategies/catalog-get-tokenuri/transformer_test.mjs
+++ test/strategies/catalog-get-tokenuri/transformer_test.mjs
@@ -10,32 +10,37 @@ import { onLine } from "../../../src/strategies/catalog-get-tokenuri/transformer
 const ajv = new Ajv();
 addFormats(ajv);
 const payload = {
-  body: {
-    artist: "MONDAY!",
-    artwork: {
-      info: {
-        mimeType: "image/jpeg",
-        uri: "https://ipfs.io/ipfs/bafybeihellebhoincjnokmu4idcdyybeztgyqzrftsbb6oxcagw5yh6qbq",
-      },
-      isNft: false,
-      nft: null,
-    },
-    duration: 98.064,
-    mimeType: "audio/wav",
-    notes:
-      "Written & Recorded by MONDAY! @musebymonday\r\nProduced & Mixed by BlackDave @blackdave\r\nMixed & Mastered by Elliott Elsey\r\nArt by @shawntelco",
-    project: null,
-    title: "Red Eye",
-    trackNumber: null,
-    version: "catalog-20210202",
-    visualizer: null,
+  metadata: {
+    tokenURI: "https://exemplarytokenURI.com",
   },
-  origin: {
-    algorithm: "secp256k1",
-    encoding: "rlp",
-    signature:
-      "0x8fbe340bc7d801a7812127dee65a3ed1adb6588318a9ba362f77607404e40c1d6579cd130924089b5f746aef221bc407d60668392ce7bc9d023c0a2f48d9e1fe1b",
-    publicKey: "0xc236541380fc0C2C05c2F2c6c52a21ED57c37952",
+  results: {
+    body: {
+      artist: "MONDAY!",
+      artwork: {
+        info: {
+          mimeType: "image/jpeg",
+          uri: "https://ipfs.io/ipfs/bafybeihellebhoincjnokmu4idcdyybeztgyqzrftsbb6oxcagw5yh6qbq",
+        },
+        isNft: false,
+        nft: null,
+      },
+      duration: 98.064,
+      mimeType: "audio/wav",
+      notes:
+        "Written & Recorded by MONDAY! @musebymonday\r\nProduced & Mixed by BlackDave @blackdave\r\nMixed & Mastered by Elliott Elsey\r\nArt by @shawntelco",
+      project: null,
+      title: "Red Eye",
+      trackNumber: null,
+      version: "catalog-20210202",
+      visualizer: null,
+    },
+    origin: {
+      algorithm: "secp256k1",
+      encoding: "rlp",
+      signature:
+        "0x8fbe340bc7d801a7812127dee65a3ed1adb6588318a9ba362f77607404e40c1d6579cd130924089b5f746aef221bc407d60668392ce7bc9d023c0a2f48d9e1fe1b",
+      publicKey: "0xc236541380fc0C2C05c2F2c6c52a21ED57c37952",
+    },
   },
 };
 const sPayload = JSON.stringify(payload);
@@ -43,8 +48,10 @@ const sPayload = JSON.stringify(payload);
 test("catalog transformer", (t) => {
   const { write } = onLine(sPayload);
   const validate = ajv.compile(track);
-  const valid = validate(JSON.parse(write));
+  const data = JSON.parse(write);
+  const valid = validate(data);
   t.is(validate.errors.length, 1);
+  t.is(data.erc721.tokenURI, payload.metadata.tokenURI);
   // NOTE: catalog-get-tokenuri implements neume-network/schema only partially
   // at this point, so we can't expect it to pass validation. Instead, we're
   // expecting errors to be thrown.
