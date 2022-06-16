4a53b10bb03f136081eb5f65cf640673eb80a135
il3ven <vaibhavchanana@yahoo.co.in>
2022-06-17 02:22:03 +0530

    test strategy name against its directory name

---
+++ test/strategy_name_test.mjs
@@ -0,0 +1,36 @@
+//@format
+import test from "ava";
+import { readdir } from "fs/promises";
+
+test("if strategy name is the same as directory name", async (t) => {
+  const source = new URL("../src/strategies", import.meta.url);
+
+  const directories = (await readdir(source, { withFileTypes: true })).filter(
+    (dirent) => dirent.isDirectory()
+  );
+
+  await Promise.all(
+    directories.map(async (dir) => {
+      const name = dir.name;
+      const extractorSource = new URL(
+        `../src/strategies/${name}/extractor.mjs`,
+        import.meta.url
+      );
+      const transformerSource = new URL(
+        `../src/strategies/${name}/transformer.mjs`,
+        import.meta.url
+      );
+
+      await Promise.all(
+        [extractorSource, transformerSource].map(async (source) => {
+          const strategy = await import(source);
+          // For better debugging abilities
+          if (!strategy.name) {
+            throw new Error(`Strategy ${name} exports no name`);
+          }
+          t.is(strategy.name, name);
+        })
+      );
+    })
+  );
+});
