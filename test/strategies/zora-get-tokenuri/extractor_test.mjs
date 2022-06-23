ded4e93f7e21266a84e7141fb0a6ffa1199fc003
Tim Daubenschütz <tim@daubenschuetz.de>
2022-06-23 10:57:08 +0200

    Fix zora-get-tokenuri unit tests for CI
    - Fixes #133

--- test/strategies/zora-get-tokenuri/extractor_test.mjs
+++ test/strategies/zora-get-tokenuri/extractor_test.mjs
@@ -1,4 +1,5 @@
 // @format
+import { env } from "process";
 import fs from "fs";
 import { resolve, dirname } from "path";
 import { fileURLToPath } from "url";
@@ -17,5 +18,17 @@ test("zora-get-tokenuri extractor", async (t) => {
   snapshot.inputs[0] = resolve(__dirname, snapshot.inputs[0]);
 
   const result = await snapshotExtractor(extractor, snapshot);
-  t.true(result.includes(snapshot.expect.write.lines[0]));
+  // NOTE: The zora-get-tokenuri strategy replaces any kind of IPFS HTTPS
+  // gateway with the gateway defined in the .env file.
+  const expected = JSON.parse(snapshot.expect.write.lines[0]);
+  delete expected.metadata.tokenURI;
+
+  let [res1, res2] = result.split("\n");
+  res1 = JSON.parse(res1);
+  t.truthy(res1.metadata.tokenURI);
+  delete res1.metadata.tokenURI;
+  res2 = JSON.parse(res2);
+  t.truthy(res2.metadata.tokenURI);
+  delete res2.metadata.tokenURI;
+  t.deepEqual(res1, expected);
 });
