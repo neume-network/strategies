9c2f6c8a7ee95d9f035a5e9e2722c7fc7b4dd654
il3ven <vaibhavchanana@yahoo.co.in>
2022-07-16 23:30:55 +0530

    crawlPath is now an input to lifecycle/init

--- src/lifecycle.mjs
+++ src/lifecycle.mjs
@@ -177,7 +177,7 @@ export function extract(strategy, worker, messageRouter, args = []) {
   });
 }
 
-export async function init(worker) {
+export async function init(worker, crawlPath) {
   const finder = await setupFinder();
   const messageRouter = new EventEmitter();
 
@@ -188,71 +188,6 @@ export async function init(worker) {
 
     messageRouter.emit(`${message.commissioner}-extraction`, message);
   });
-
-  // crawlPath[i] and crawlPath[i+1] are executed in sequence
-  // crawlPath[i][j] and crawlPath[i][j+1] are executed in parallel
-  // TODO: Define and check for valid message schema. Current lifecycle message schema
-  // doesn't work. https://github.com/neume-network/message-schema/issues/19
-  const crawlPath = [
-    [{ name: "web3subgraph", extractor: {}, transform: {} }],
-    [
-      {
-        name: "soundxyz-call-tokenuri",
-        extractor: {
-          args: [resolve(env.DATA_DIR, "web3subgraph-transformation")],
-        },
-        transformer: {},
-      },
-      {
-        name: "zora-call-tokenuri",
-        extractor: {
-          args: [resolve(env.DATA_DIR, "web3subgraph-transformation")],
-        },
-        transformer: {},
-      },
-      {
-        name: "zora-call-tokenmetadatauri",
-        extractor: {
-          args: [resolve(env.DATA_DIR, "web3subgraph-transformation")],
-        },
-        transformer: {},
-      },
-      {
-        name: "soundxyz-metadata",
-        extractor: {
-          args: [resolve(env.DATA_DIR, "web3subgraph-transformation")],
-        },
-        transformer: {},
-      },
-    ],
-    [
-      {
-        name: "soundxyz-get-tokenuri",
-        extractor: {
-          args: [
-            resolve(env.DATA_DIR, "soundxyz-call-tokenuri-transformation"),
-          ],
-        },
-        transformer: {},
-      },
-      {
-        name: "zora-get-tokenuri",
-        extractor: {
-          args: [
-            resolve(env.DATA_DIR, "zora-call-tokenmetadatauri-transformation"),
-          ],
-        },
-        transformer: {},
-      },
-    ],
-    [
-      {
-        name: "music-os-accumulator",
-        extractor: { args: [] },
-        transformer: {},
-      },
-    ],
-  ];
 
   for await (const path of crawlPath) {
     await Promise.all(
@@ -289,4 +224,10 @@ export async function init(worker) {
       })
     );
   }
+
+  log("All strategies executed");
+  worker.postMessage({
+    type: "exit",
+    version: "0.0.1",
+  });
 }
