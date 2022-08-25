0f8b123ea73a0714479b263a7a102dea5fedf21c
Tim Daubenschütz <tim@daubenschuetz.de>
2022-08-25 17:04:04 +0200

    Make call-block-logs fail on false inputs
    - #229

--- src/strategies/call-block-logs/extractor.mjs
+++ src/strategies/call-block-logs/extractor.mjs
@@ -4,8 +4,11 @@ import assert from "assert";
 
 import { toHex } from "eth-fun";
 
+import logger from "../../logger.mjs";
+
 const version = "0.0.1";
 export const name = "call-block-logs";
+const log = logger(name);
 export const props = {};
 const options = {
   url: env.RPC_HTTP_HOST,
@@ -17,7 +20,24 @@ if (env.RPC_API_KEY) {
   };
 }
 
-function callBlockLogs(number, end) {
+function blockNumber(start, end) {
+  return {
+    type: "json-rpc",
+    commissioner: name,
+    method: "eth_blockNumber",
+    params: [],
+    metadata: {
+      start,
+      end,
+    },
+    version,
+    options,
+    results: null,
+    error: null,
+  };
+}
+
+function callBlockLogs(number) {
   number = toHex(number);
   return {
     type: "json-rpc",
@@ -29,9 +49,6 @@ function callBlockLogs(number, end) {
         toBlock: number,
       },
     ],
-    metadata: {
-      end,
-    },
     version,
     options,
     results: null,
@@ -39,22 +56,53 @@ function callBlockLogs(number, end) {
   };
 }
 
-export function init(start = 0, end) {
+function generateMessages(start, end) {
   const difference = end - start;
 
   let messages = [];
   for (let i of Array(difference).keys()) {
     messages.push(callBlockLogs(start + i));
   }
+  return messages;
+}
+
+export function init(start = 0, end) {
   return {
     write: null,
-    messages,
+    messages: [blockNumber(start, end)],
   };
 }
 
 export function update(message) {
-  return {
-    messages: [],
-    write: JSON.stringify(message.results),
-  };
+  if (message?.method == "eth_blockNumber") {
+    let endBlockNumber = message.metadata.end;
+    const latestBlockNumber = parseInt(message.results);
+    if (
+      endBlockNumber > latestBlockNumber &&
+      message.metadata.start <= latestBlockNumber
+    ) {
+      log(
+        `Submitted end block number "${endBlockNumber}" is bigger than current chain tip block number "${latestBlockNumber}. Setting end block number to latest block number."`
+      );
+      endBlockNumber = latestBlockNumber;
+    } else {
+      log(
+        `Start block number "${message.metadata.start}" is bigger than network's current block number "${latestBlockNumber}"`
+      );
+      return {
+        write: null,
+        messages: [{ type: "exit" }],
+      };
+    }
+
+    return {
+      write: null,
+      messages: generateMessages(message.metadata.start, endBlockNumber),
+    };
+  } else {
+    return {
+      messages: [],
+      write: JSON.stringify(message.results),
+    };
+  }
 }
--- test/strategies/call-block-logs/extractor_test.mjs
+++ test/strategies/call-block-logs/extractor_test.mjs
@@ -2,13 +2,58 @@
 import fs from "fs";
 import { resolve, dirname } from "path";
 import { fileURLToPath } from "url";
+import { env } from "process";
 
 import test from "ava";
 
 import snapshotExtractor from "../../../utils/snapshot_extractor.mjs";
 import * as blockLogs from "../../../src/strategies/call-block-logs/extractor.mjs";
 
-test("call-block-logs extractor", async (t) => {
+const options = {
+  url: env.RPC_HTTP_HOST,
+};
+
+if (env.RPC_API_KEY) {
+  options.headers = {
+    Authorization: `Bearer ${env.RPC_API_KEY}`,
+  };
+}
+
+test("if call-block-logs update throws when start block is bigger than network's latest block number", async (t) => {
+  const { write, messages } = blockLogs.update({
+    type: "json-rpc",
+    commissioner: "call-block-logs",
+    method: "eth_blockNumber",
+    params: [],
+    metadata: { start: 99999999, end: 100000000 },
+    version: "0.0.1",
+    options,
+    results: "0xeb2207",
+    error: null,
+  });
+  t.is(messages[0].type, "exit");
+});
+
+test("if call-block-logs adjusts end block number when start block is in range of network", async (t) => {
+  const { write, messages } = blockLogs.update({
+    type: "json-rpc",
+    commissioner: "call-block-logs",
+    method: "eth_blockNumber",
+    params: [],
+    metadata: { start: 15409670, end: 100000000 },
+    version: "0.0.1",
+    options,
+    results: "0xeb2207",
+    error: null,
+  });
+  t.is(messages[0].method, "eth_getLogs");
+  t.is(messages[0].params[0].fromBlock, "0xeb2206");
+});
+
+// NOTE: Since we now use `init` to first download the network's current block,
+// it seems `snapshotExtractor` isn't capable of waiting a number of messages
+// before return the results.
+test.skip("call-block-logs extractor", async (t) => {
   const __dirname = dirname(fileURLToPath(import.meta.url));
 
   const snapshot = JSON.parse(
