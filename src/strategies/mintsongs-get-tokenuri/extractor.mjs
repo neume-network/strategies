8c863772360148af7139164f3fb65252cfc56628
il3ven <vaibhavchanana@yahoo.co.in>
2022-09-05 02:08:02 +0530

    add mintsongs-call-tokenuri and mintsongs-get-tokenuri
    For mintsongs-get-tokenuri, this commit uses a strategy factory.
    
    The factory can later be reused for zora contracts.

---
+++ src/strategies/mintsongs-get-tokenuri/extractor.mjs
@@ -0,0 +1,10 @@
+// @format
+import { getIpfsTokenUriFactory } from "../../strategy-factories/get-ipfs-tokenuri/extractor.mjs";
+
+export const version = "0.0.1";
+export const name = "mintsongs-get-tokenuri";
+export const props = { strategyName: name, version, options: {} };
+
+const { init, update } = getIpfsTokenUriFactory(props);
+
+export { init, update };
