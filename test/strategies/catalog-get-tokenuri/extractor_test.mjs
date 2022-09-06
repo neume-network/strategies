a3c44d3e540883ae022a6ae668b83fdd8880bd85
Tim Daubenschütz <tim@daubenschuetz.de>
2022-09-06 17:40:38 +0200

    In catalog-get-tokenuri test ignore tokenuri

--- test/strategies/catalog-get-tokenuri/extractor_test.mjs
+++ test/strategies/catalog-get-tokenuri/extractor_test.mjs
@@ -17,5 +17,15 @@ test("catalog-get-tokenuri extractor", async (t) => {
   snapshot.inputs[0] = resolve(__dirname, snapshot.inputs[0]);
 
   const result = await snapshotExtractor(catalog, snapshot);
-  t.deepEqual(JSON.parse(result), JSON.parse(snapshot.expect.write));
+  const pResult = JSON.parse(result);
+  const pExpected = JSON.parse(snapshot.expect.write);
+  // NOTE: Given different configurations for `env.IPFS_HTTP_GATEWAY` the
+  // tokenURI may be different from instance to instance, and will fail a
+  // direct comparison in tests. Hence we just check if the `tokenURI` is
+  // indeed present but otherwise ignore its value check.
+  t.truthy(pResult.metadata.tokenURI);
+  t.truthy(pExpected.metadata.tokenURI);
+  delete pResult.metadata.tokenURI;
+  delete pExpected.metadata.tokenURI;
+  t.deepEqual(pResult, pExpected);
 });
