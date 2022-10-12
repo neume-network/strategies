dcb94a91ca1b1736e82970e2957f7588a29c46fc
il3ven <vaibhavchanana@yahoo.co.in>
2022-10-13 01:46:03 +0530

    add `breakdownIpfs` function
    This function breaks down any IPFS URL into CID and path.
    
    The previously added `anyIpfsToNativeIpfs` now uses `breakdownIpfs`.

--- src/utils.mjs
+++ src/utils.mjs
@@ -21,30 +21,49 @@ export function parseJSON(value, distance = 10) {
   return parsed;
 }
 
-export function anyIpfsToNativeIpfs(ipfsUri) {
-  const IPFSIANAScheme = "ipfs://";
+export function breakdownIpfs(ipfsUri) {
   // Modified from is-ipfs package at https://github.com/ipfs-shipyard/is-ipfs/blob/393859af921ef52d786c0f14bf772eeda7f8930b/src/index.js#L12
+  const nativeIpfsPattern = /ipfs:\/\/([^/?#]+)(.*)/;
   const pathGatewayPattern = /^https?:\/\/[^/]+\/ipfs\/([^/?#]+)(.*)/;
   const subdomainGatewayPattern = /^https?:\/\/([^/]+)\.ipfs\.[^/?#]+(.*)/;
 
   if (typeof ipfsUri !== "string")
     throw new Error("Given IPFS URI should be of type string");
 
-  if (ipfsUri.startsWith(IPFSIANAScheme)) return ipfsUri;
+  const nativeIpfsMatch = ipfsUri.match(nativeIpfsPattern);
+  if (nativeIpfsMatch) {
+    const [_, hash, path] = nativeIpfsMatch;
+    return {
+      cid: hash,
+      path: path,
+    };
+  }
 
   const pathGatewayMatch = ipfsUri.match(pathGatewayPattern);
   if (pathGatewayMatch) {
     const [_, hash, path] = pathGatewayMatch;
-    return `${IPFSIANAScheme}${hash}${path}`;
+    return {
+      cid: hash,
+      path: path,
+    };
   }
 
   const subdomainGatewayMatch = ipfsUri.match(subdomainGatewayPattern);
   if (subdomainGatewayMatch) {
     const [_, hash, path] = subdomainGatewayMatch;
-    return `${IPFSIANAScheme}${hash}${path}`;
+    return {
+      cid: hash,
+      path: path,
+    };
   }
 
   throw new Error(`Couldn't convert ${ipfsUri} to native IPFS URI`);
+}
+
+export function anyIpfsToNativeIpfs(ipfsUri) {
+  const IPFSIANAScheme = "ipfs://";
+  const ipfs = breakdownIpfs(ipfsUri);
+  return `${IPFSIANAScheme}${ipfs.cid}${ipfs.path}`;
 }
 
 export function ifIpfsConvertToNativeIpfs(uri) {
--- test/utils_test.mjs
+++ test/utils_test.mjs
@@ -1,6 +1,11 @@
 import test from "ava";
 
-import { parseJSON, anyIpfsToNativeIpfs } from "../src/utils.mjs";
+import {
+  parseJSON,
+  anyIpfsToNativeIpfs,
+  breakdownIpfs,
+  ifIpfsConvertToNativeIpfs,
+} from "../src/utils.mjs";
 
 test("json parser parses", (t) => {
   const data = '{"hello": "world", "invalid": "string"}';
@@ -28,37 +33,100 @@ test("json parser can handle big distance", (t) => {
   }
 });
 
-function ipfsTest(t, input, expected) {
-  t.is(anyIpfsToNativeIpfs(input), expected);
+function ipfsTest(t, ipfsUri, breakdown, nativeUriString) {
+  t.deepEqual(breakdownIpfs(ipfsUri), breakdown);
+  t.is(anyIpfsToNativeIpfs(ipfsUri), nativeUriString);
+  t.is(ifIpfsConvertToNativeIpfs(ipfsUri), nativeUriString);
 }
 
 [
   [
     "https://soundxyz.mypinata.cloud/ipfs/QmXHfjwQ1MKmZLmzMNWc51brBTxFRWyjXKtLLysj2e5FcU",
+    { cid: "QmXHfjwQ1MKmZLmzMNWc51brBTxFRWyjXKtLLysj2e5FcU", path: "" },
     "ipfs://QmXHfjwQ1MKmZLmzMNWc51brBTxFRWyjXKtLLysj2e5FcU",
   ],
   [
     "https://neume.infura-ipfs.io/ipfs/bafybeihehacot7c5dj7y5qhwgaj7q6gdxfrjfmxcltl6wmtrjxzcn6cs7e",
+    {
+      cid: "bafybeihehacot7c5dj7y5qhwgaj7q6gdxfrjfmxcltl6wmtrjxzcn6cs7e",
+      path: "",
+    },
     "ipfs://bafybeihehacot7c5dj7y5qhwgaj7q6gdxfrjfmxcltl6wmtrjxzcn6cs7e",
   ],
   [
     "https://neume.infura-ipfs.io/ipfs/bafybeihehacot7c5dj7y5qhwgaj7q6gdxfrjfmxcltl6wmtrjxzcn6cs7e/path/to/",
+    {
+      cid: "bafybeihehacot7c5dj7y5qhwgaj7q6gdxfrjfmxcltl6wmtrjxzcn6cs7e",
+      path: "/path/to/",
+    },
     "ipfs://bafybeihehacot7c5dj7y5qhwgaj7q6gdxfrjfmxcltl6wmtrjxzcn6cs7e/path/to/",
   ],
   [
     "https://ipfs.fleek.co/ipfs/bafybeiamien6u7hqvnij4ylfz76hcv3ojshzmdklsfguz42krrs6mpaubm/",
+    {
+      cid: "bafybeiamien6u7hqvnij4ylfz76hcv3ojshzmdklsfguz42krrs6mpaubm",
+      path: "/",
+    },
     "ipfs://bafybeiamien6u7hqvnij4ylfz76hcv3ojshzmdklsfguz42krrs6mpaubm/",
   ],
   [
     "https://bafybeiemxf5abjwjbikoz4mc3a3dla6ual3jsgpdr4cjr3oz3evfyavhwq.ipfs.dweb.link",
+    {
+      cid: "bafybeiemxf5abjwjbikoz4mc3a3dla6ual3jsgpdr4cjr3oz3evfyavhwq",
+      path: "",
+    },
     "ipfs://bafybeiemxf5abjwjbikoz4mc3a3dla6ual3jsgpdr4cjr3oz3evfyavhwq",
   ],
   [
     "https://bafybeiemxf5abjwjbikoz4mc3a3dla6ual3jsgpdr4cjr3oz3evfyavhwq.ipfs.cf-ipfs.com/wiki/Vincent_van_Gogh.html",
+    {
+      cid: "bafybeiemxf5abjwjbikoz4mc3a3dla6ual3jsgpdr4cjr3oz3evfyavhwq",
+      path: "/wiki/Vincent_van_Gogh.html",
+    },
     "ipfs://bafybeiemxf5abjwjbikoz4mc3a3dla6ual3jsgpdr4cjr3oz3evfyavhwq/wiki/Vincent_van_Gogh.html",
   ],
+  [
+    "https://bafybeiemxf5abjwjbikoz4mc3a3dla6ual3jsgpdr4cjr3oz3evfyavhwq.ipfs.cf-ipfs.com/wiki/Vincent_van_Gogh.html/#1850",
+    {
+      cid: "bafybeiemxf5abjwjbikoz4mc3a3dla6ual3jsgpdr4cjr3oz3evfyavhwq",
+      path: "/wiki/Vincent_van_Gogh.html/#1850",
+    },
+    "ipfs://bafybeiemxf5abjwjbikoz4mc3a3dla6ual3jsgpdr4cjr3oz3evfyavhwq/wiki/Vincent_van_Gogh.html/#1850",
+  ],
+  [
+    "https://ipfs.io/ipfs/bafybeiemxf5abjwjbikoz4mc3a3dla6ual3jsgpdr4cjr3oz3evfyavhwq#1850",
+    {
+      cid: "bafybeiemxf5abjwjbikoz4mc3a3dla6ual3jsgpdr4cjr3oz3evfyavhwq",
+      path: "#1850",
+    },
+    "ipfs://bafybeiemxf5abjwjbikoz4mc3a3dla6ual3jsgpdr4cjr3oz3evfyavhwq#1850",
+  ],
+  [
+    "https://ipfs.io/ipfs/bafybeiemxf5abjwjbikoz4mc3a3dla6ual3jsgpdr4cjr3oz3evfyavhwq/#1850",
+    {
+      cid: "bafybeiemxf5abjwjbikoz4mc3a3dla6ual3jsgpdr4cjr3oz3evfyavhwq",
+      path: "/#1850",
+    },
+    "ipfs://bafybeiemxf5abjwjbikoz4mc3a3dla6ual3jsgpdr4cjr3oz3evfyavhwq/#1850",
+  ],
+  [
+    "ipfs://bafybeihehacot7c5dj7y5qhwgaj7q6gdxfrjfmxcltl6wmtrjxzcn6cs7e/path/to/",
+    {
+      cid: "bafybeihehacot7c5dj7y5qhwgaj7q6gdxfrjfmxcltl6wmtrjxzcn6cs7e",
+      path: "/path/to/",
+    },
+    "ipfs://bafybeihehacot7c5dj7y5qhwgaj7q6gdxfrjfmxcltl6wmtrjxzcn6cs7e/path/to/",
+  ],
+  [
+    "ipfs://bafybeihehacot7c5dj7y5qhwgaj7q6gdxfrjfmxcltl6wmtrjxzcn6cs7e/",
+    {
+      cid: "bafybeihehacot7c5dj7y5qhwgaj7q6gdxfrjfmxcltl6wmtrjxzcn6cs7e",
+      path: "/",
+    },
+    "ipfs://bafybeihehacot7c5dj7y5qhwgaj7q6gdxfrjfmxcltl6wmtrjxzcn6cs7e/",
+  ],
 ].forEach((args) =>
-  test(`valid anyIpfsToNativeIpfs - ${args[0]}`, ipfsTest, args[0], args[1])
+  test(`valid anyIpfsToNativeIpfs - ${args[0]}`, ipfsTest, ...args)
 );
 
 test("anyIpfsToNativeIpfs should throw error on invalid IPFS URI", (t) => {
